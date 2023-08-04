389f90f681b7ed709218ad48068b4f21e256c31b
wsbblog <wsbblog@outlook.com>
2023-08-04 14:07:20 +0800

    Site updated: 2023-08-04 14:07:17

--- js/tianligpt.js
+++ js/tianligpt.js
@@ -1,1 +1,3 @@
-let tianliGPT_postSelector = '\#post \#article-container';let tianliGPT_key = 'xFaxqBWLI3Z5um7le2Aq';
+let tianliGPT_postSelector = '#post #article-container';
+let tianliGPT_key = 'xFaxqBWLI3Z5um7le2Aq';
+
