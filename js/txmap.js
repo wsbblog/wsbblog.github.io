d85095a18d64b709787caa9493e30d55ec016ee2
wsbblog <wsbblog@outlook.com>
2023-08-20 15:03:37 +0800

    Site updated: 2023-08-20 15:03:34

--- js/txmap.js
+++ js/txmap.js
@@ -1,14 +1,9 @@
 window.addEventListener('DOMContentLoaded', function() {
-    var navMusic = document.getElementById('nav-music');
-    var fpsElement = document.getElementById('fps');
-  
-    var bottomBar = document.createElement('div');
-    bottomBar.className = 'bottom-bar';
-  
-    // 将navMusic元素的父元素替换为bottomBar元素
-    navMusic.parentNode.replaceChild(bottomBar, navMusic);
-  
-    // 在bottomBar元素中插入navMusic和fpsElement元素
-    bottomBar.appendChild(navMusic);
-    bottomBar.appendChild(fpsElement);
+      var fpsElement = document.getElementById('fps');
+      var parentElement = fpsElement.parentElement;
+      var bottomBar = document.createElement('div');
+      bottomBar.className = 'bottom-bar';
+      parentElement.insertBefore(bottomBar, fpsElement);
+      bottomBar.appendChild(fpsElement);
   });
+  
